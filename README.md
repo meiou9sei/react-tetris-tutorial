00fba3fbf18271e69afed1d38fea7fe559226b15
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-14 20:27:20 -0400

    create collision detection

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 codealong tutorial https://www.youtube.com/watch?v=ZGOaCxX8HIU
 
 goal: gain deeper understanding of react hooks
+
+note: this does not use react strict mode (breaks if implemented - tutorial vid is old and breaks some rules, apparently)
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledDisplay } from './styles/StyledDisplay';
 
 const Display = ({ gameOver, text}) => (
-    <StyledDisplay gameover={gameOver}>{text}</StyledDisplay>
+    <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
 );
 
 export default Display;
