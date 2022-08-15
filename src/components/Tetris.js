0bfa09a9d8706a3ac6a4d8802c89febe03657846
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-15 12:13:59 -0400

    implement drop interval

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 //custom hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -32,6 +33,7 @@ const Tetris = () => {
     const startGame = () => {
         //reset everything
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
     }
@@ -50,7 +52,17 @@ const Tetris = () => {
         }
     }
 
+    const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 40) {
+                console.log('interval on');
+                setDropTime(1000);
+            }
+        }
+    }
     const dropPlayer = () => {
+        console.log('interval off');
+        setDropTime(null);
         drop();
     }
 
@@ -68,8 +80,17 @@ const Tetris = () => {
         }
     }
 
+    useInterval(() => {
+        drop();
+    }, dropTime)
+
     return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper
+            role="button" 
+            tabIndex="0" 
+            onKeyDown={e => move(e)} 
+            onKeyUp={keyUp}
+        >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
